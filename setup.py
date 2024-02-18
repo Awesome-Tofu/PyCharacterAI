928e2c2ba5e94bf9b762203e6766e1ba1cf3c885
awesome-tofu <adityaraj6311@gmail.com>
2024-02-18 16:19:32 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r', encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name='PyCharacterAI',
+    version='1.1.0',
+    author='XtraF',
+    author_email='igoromarov15@gmail.com',
+    description='An unofficial asynchronous api wrapper for Character AI.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Xtr4F/PyCharacterAI',
+    packages=find_packages(),
+    install_requires=['uuid>=1.30',
+                      'curl-cffi==0.5.9'],
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License'
+    ],
+    python_requires='>=3.7'
+)
+
